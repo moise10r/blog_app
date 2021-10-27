55c9786bf40abe4ee6306c1404a0d07c3bb61297
moise10r <moiserushanika2006@gmail.com>
2021-10-27 21:45:39 +0200

    Add the show action in usercontroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "users#index"
-  resources :users
+  resources :users, only: [:index, :show]
   # get "users",to: "users#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
