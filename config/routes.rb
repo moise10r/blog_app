3204bcbbda65ee31eca75dedd2ba3a00ea8bb311
moise10r <moiserushanika2006@gmail.com>
2021-10-27 21:49:45 +0200

    Add the show action in router

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "users#index"
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
   # get "users",to: "users#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
