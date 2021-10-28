5840908af4c0e15f7fdea335557ee72f288e7a48
moise10r <moiserushanika2006@gmail.com>
2021-10-29 00:06:32 +0200

    Add the index action test for usercontroller

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  describe 'GET #index' do
+    before(:example) { get users_path }
+
+    it 'should have correct response status' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'should render correct template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'should include correct placeholder text' do
+      expect(response.body).to include('Here there is a list of users')
+    end
+  end
+
+
+end
