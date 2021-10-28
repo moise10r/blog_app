2b244ed3f567147ede92d0e66c16a3ed975ae524
moise10r <moiserushanika2006@gmail.com>
2021-10-29 00:06:53 +0200

    Add the show action test for usercontroller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -17,5 +17,19 @@ RSpec.describe 'Users', type: :request do
     end
   end
 
+  describe 'GET #show' do
+    before(:example) { get user_path(10) }
 
+    it 'should have correct response status' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'should render correct template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'should include correct placeholder text' do
+      expect(response.body).to include('Show the details of one user')
+    end
+  end
 end
