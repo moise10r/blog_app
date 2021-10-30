1425c02618008d9d02a59f33a9ad0056a83338cf
moise10r <moiserushanika2006@gmail.com>
2021-10-30 13:34:28 +0200

    Remove postcounter from post table.

--- db/migrate/20211030112903_create_posts.rb
+++ db/migrate/20211030112903_create_posts.rb
@@ -3,9 +3,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
     create_table :posts do |t|
       t.string :title
       t.text :text
-      t.integer :commentsCounter
-      t.integer :likesCounter
-
+      
       t.timestamps
     end
   end
