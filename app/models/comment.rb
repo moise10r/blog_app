d15a0814cd3d117b4a484960c76e290dc242d5ae
moise10r <moiserushanika2006@gmail.com>
2021-11-02 01:18:56 +0200

    Update methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :post
   validates :text, presence: true
 
-  def update_counter(post_id)
+  def self.update_counter(post_id)
     count_comments = Comment.where(post_id: post_id).count
     Post.find_by(id: post_id).update(comments_counter: count_comments)
   end
