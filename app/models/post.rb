6a1b4b33a93546cef2858938ffb7ed9f47b11092
moise10r <moiserushanika2006@gmail.com>
2021-11-02 00:45:08 +0200

    Add update counter method on post class model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  has_many :comments
+  has_many :likes
+  validates :title, :text, presence: true
+
+  def update_counter(author_id)
+    count_posts = Post.where(author_id: author_id).count
+    User.find_by(id: author_id).update(post_counter: count_posts)
+  end
+
+
 end
