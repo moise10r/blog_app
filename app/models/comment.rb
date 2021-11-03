4361e32aea4ce1ace5536a2a4ca15660f69e773e
moise10r <moiserushanika2006@gmail.com>
2021-11-03 22:39:43 +0200

    Fix linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :post
   validates :text, presence: true
 
-
   def update_post_comments_counter
     post.update_column('comments_counter', post.comments_counter + 1)
   end
