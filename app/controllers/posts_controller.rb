f32fbbbfdb258af0056c90a5c4db2659ebef7895
moise10r <moiserushanika2006@gmail.com>
2021-11-03 21:04:14 +0200

    Add index and show method in post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,10 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @auh_id = params[:author_id]
+  end
 
-  def show; end
+  def show
+    @author_id = params[:author_id]
+    @post_id = params[:id]
+  end
 end
