1af6a3653b4eddb3606c98de963912714a96fd07
moise10r <moiserushanika2006@gmail.com>
2021-11-05 19:54:17 +0200

    Add new routers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'users#index'
+  post 'users/:id/like/create', to: 'likes#create', as: 'post_like_create'
+  post 'users/:id/comment/create', to: 'comments#create', as: 'post_comment_create'
+  get 'users/:author_id/post/new', to: 'posts#new', as: 'user_post_new'
+  post 'users/:author_id/post', to: 'posts#create', as: 'user_post_create'
   get 'users/:author_id/posts', to: 'posts#index', as: 'user_posts'
   get 'users/:author_id/posts/:id', to: 'posts#show', as: 'user_post'
   get 'users', to: 'users#index', as: 'users'
