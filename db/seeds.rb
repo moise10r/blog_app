103afb98ef2d82aa223692dddceac7640afd5cd4
moise10r <moiserushanika2006@gmail.com>
2021-11-05 22:31:15 +0200

    Fix bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(name:"Moise")
-User.create(name:"Rushanika")
-User.create(name:"Nganulo")
