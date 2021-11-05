0ce1382914a4db0157e1fe295379f94738c48b6a
moise10r <moiserushanika2006@gmail.com>
2021-11-05 23:56:29 +0200

    Fix comment display bug

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  validates :text, presence: true
 
   def update_post_comments_counter
     post.update_column('comments_counter', post.comments_counter + 1)
