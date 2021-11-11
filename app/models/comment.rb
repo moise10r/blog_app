afaa166ee4a82d423e36ca1c836b4a401698ab12
moise10r <moiserushanika2006@gmail.com>
2021-11-12 01:51:38 +0200

    Add trim method.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,16 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
+  after_create :update_post_comments_counter
+  before_validation :trim_text
+
   def update_post_comments_counter
     post.update_column('comments_counter', post.comments_counter + 1)
+  end
+
+  private
+
+  def trim_text
+    self.text = text.strip if text.present?
   end
 end
