fda78b61b26d8406e2ae3090fe2d5b9de38874bf
moise10r <moiserushanika2006@gmail.com>
2021-11-12 01:30:16 +0200

    Get post by post id.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,7 @@
 class CommentsController < ApplicationController
   def create
-    post = Post.find(params[:id])
+    post = Post.find(params[:post_id])
     new_comment = Comment.create(user_id: current_user.id, post_id: post.id, text: comment_params[:text])
-    new_comment.update_post_comments_counter
     if new_comment.save
       flash[:notice] = 'Comment created!'
       redirect_to user_post_path(post.user.id, post.id)
