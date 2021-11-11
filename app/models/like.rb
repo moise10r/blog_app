41e115756c242beb8cc2b5be0d9f6a3e5ac220a8
moise10r <moiserushanika2006@gmail.com>
2021-11-12 01:52:24 +0200

    Add after method.

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,8 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
+  after_create :update_post_likes_counter
+
   def update_post_likes_counter
     post.update_column('likes_counter', post.likes_counter + 1)
   end
