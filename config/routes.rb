45a29eef976206f3f8659b982512f5b68d7b48dc
moise10r <moiserushanika2006@gmail.com>
2021-11-12 02:32:40 +0200

    Change router

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    confirmations: 'confirmations'
+  }, path_names: {
+    sign_in: 'log_in',
+    sign_up: 'create_account',
+    sign_out: 'logout'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'users#index'
-  post 'users/:id/like/create', to: 'likes#create', as: 'post_like_create'
-  post 'users/:id/comment/create', to: 'comments#create', as: 'post_comment_create'
-  get 'users/:user_id/post/new', to: 'posts#new', as: 'user_post_new'
-  post 'users/:user_id/post', to: 'posts#create', as: 'user_post_create'
-  get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-  get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
-  get 'users', to: 'users#index', as: 'users'
-  get 'users/:id', to: 'users#show', as: 'user'
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
+
+  resources :posts, only: [:new, :create] do
+    resources :comments, only: [:create]
+    resources :likes, only: [:create]
+  end
 end
