7858a2de0735f7f05d1f054946aea1e41889b6fd
moise10r <moiserushanika2006@gmail.com>
2021-11-12 02:29:44 +0200

    set letteropen config file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-require 'active_support/core_ext/integer/time'
+require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -35,8 +35,6 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
 
   config.action_mailer.perform_caching = false
 
@@ -75,4 +73,21 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  # config.after_initialize do
+  #   Bullet.enable = true
+  #   Bullet.alert = ENV['BULLET_ALERT']
+  #   Bullet.bullet_logger = true
+  #   Bullet.console = true
+  #   Bullet.rails_logger = true
+  # end
+
+  # devisie config settings
+    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+    config.action_mailer.perform_deliveries = true
+
+  # letter opener config
+  config.action_mailer.delivery_method = :letter_opener
+  LetterOpener.configure do |config|
+    config.message_template = :light
+  end 
 end
